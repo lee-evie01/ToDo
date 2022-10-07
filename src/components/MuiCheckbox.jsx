3411364a0a5df8abc419bb101cd7a3de6ced9db5
Patrick Hultquist <patrick@monogram.io>
2022-10-06 21:38:40 -0400

    prevent form submission

--- src/components/MuiCheckbox.jsx
+++ src/components/MuiCheckbox.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import {Box, FormControlLabel, Checkbox} from '@material-ui/core/'
+import React from "react";
+import { Box, FormControlLabel, Checkbox } from "@material-ui/core/";
 
 const MuiCheckbox = () => {
+  return (
+    <Box>
+      <Box>
+        <FormControlLabel
+          label="I accept terms and conditions"
+          control={<Checkbox />}
+        />
+      </Box>
+    </Box>
+  );
+};
 
-    return (
-        <Box>
-            <Box>
-                <FormControlLabel 
-                label= 'I accept terms and conditions'
-                control = {< Checkbox />}
-                />
-            </Box>
-        </Box>
-    )
-}
-
-export default MuiCheckbox
+export default MuiCheckbox;
--- src/components/MuiTextfield.jsx
+++ src/components/MuiTextfield.jsx
@@ -1,27 +1,34 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { useState } from 'react';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
-export default function MuiTextfield({onChange, val}) {
+export default function MuiTextfield({ onChange, val }) {
   return (
     <Box
       component="form"
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
       sx={{
-        '& > :not(style)': { m: .01, width: '25ch' },
+        "& > :not(style)": { m: 0.01, width: "25ch" },
       }}
       noValidate
       autoComplete="off"
     >
-        {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
-        {/* <TextField id="filled-basic" label="Filled" variant="filled" /> */}
-      <TextField id="standard-basic" label=" " variant="standard" onChange={onChange} val={val} />
-      
+      {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
+      {/* <TextField id="filled-basic" label="Filled" variant="filled" /> */}
+      <TextField
+        id="standard-basic"
+        label=" "
+        variant="standard"
+        onChange={onChange}
+        val={val}
+      />
 
       {/* <input value = {val} onChange ={(e)=>{setVal(e.target.value)}} /> */}
     </Box>
-
   );
-  
+
   console.log(val);
 }
