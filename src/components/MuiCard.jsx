605c48f55ee76b726c0cd4b7aaac1505a97f515d
Patrick Hultquist <patrick@monogram.io>
2022-10-06 21:19:50 -0400

    move typography

--- src/components/MuiCard.jsx
+++ src/components/MuiCard.jsx
@@ -1,18 +1,18 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import MuiTextfield from './MuiTextfield';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import MuiTextfield from "./MuiTextfield";
 // import MuiDate from './MuiDate';
-import { useState } from 'react';
+import { useState } from "react";
 
 const bull = (
   <Box
     component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
   >
     •
   </Box>
@@ -25,35 +25,27 @@ export default function MuiCard() {
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        
         <Typography sx={{ fontSize: 14 }} color="text.secondary">
           Title
-          <MuiTextfield onChange={(e) => setVal(e.target.value)} val = {val}/>
-          
         </Typography>
+        <MuiTextfield onChange={(e) => setVal(e.target.value)} val={val} />
+
         <br></br>
         <Typography sx={{ fontSize: 14 }} color="text.secondary">
           Tags
-          
-          <MuiTextfield />
-          <Button size="small">Add</Button>
         </Typography>
+        <MuiTextfield />
+        <Button size="small">Add</Button>
         <br></br>
         <Typography sx={{ fontSize: 14 }} color="text.secondary">
           Due Date
-          
-          <MuiTextfield />
         </Typography>
-          
-        <br/>
-        <Button onClick = {() => setCreate(val)}>Create</Button>
-        
+        <MuiTextfield />
+
+        <br />
+        <Button onClick={() => setCreate(val)}>Create</Button>
       </CardContent>
-      <CardActions>
-      </CardActions>
+      <CardActions></CardActions>
     </Card>
   );
 }
-
-
-
